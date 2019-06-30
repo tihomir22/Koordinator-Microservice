c55ec9e1c2f5854b9aa647662c92e03078bc5759
tihomir22 <tihomir_alcudia3@hotmail.com>
2019-06-30 21:59:42 +0200

    dembow

--- src/main/java/com/koordinator/epsilon/Koordinator/controladores/ControladorPrecios.java
+++ src/main/java/com/koordinator/epsilon/Koordinator/controladores/ControladorPrecios.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.util.*;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 public class ControladorPrecios {
     @Autowired
     private RepositorioActivos repositorioActivos;
