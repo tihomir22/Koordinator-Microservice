f983925476edf50a742a3fc0e6334eca1e8f6a9b
Tihomir Stoychev Stoychev <tihomir_alcudia3@hotmail.com>
2019-08-22 13:07:28 +0200

    subida a heroku, estable

--- src/main/java/com/koordinator/epsilon/Koordinator/entidades/TechnicalRegistry.java
+++ src/main/java/com/koordinator/epsilon/Koordinator/entidades/TechnicalRegistry.java
@@ -27,6 +27,9 @@ public class TechnicalRegistry {
         this.date = date;
     }
 
+    public TechnicalRegistry() {
+    }
+
     @Override
     public String toString() {
         return "TechnicalRegistry{" +
